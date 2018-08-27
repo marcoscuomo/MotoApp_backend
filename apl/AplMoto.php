80dc7b4f932419826ce9c98b2e99e0d5ce61c925
Marcos Souza <marcos@mojumob.com.br>
2018-08-26 23:31:30 -0300

    Cria a classe ApIMoto

---
+++ apl/AplMoto.php
@@ -0,0 +1,22 @@
+include '../data/Database.php';
+
+class AplMoto
+{
+    public static function getMotosComoJson(){
+        $motos = Database::getMotos( '../data/motos.json' );
+        return json_encode( $motos );
+    }
+
+    public static function updateFavoritoMoto( Moto $moto ){
+        $motos = Database::getMotos( '../data/motos.json' );
+
+        foreach( $motos as $m ){
+            if( $m->id == $moto->getId() ){
+                $m->ehFavorito = $moto->getEhFavorito();
+                Database::saveDatabase( '../data/motos.json', $motos );
+                break;
+            }
+        }
+        return json_encode( $moto );
+    }
+}
